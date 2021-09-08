6f97c71c43c7836e43d7a80dd320a0528f318dc1
GRaviTeja25 <89765454+GRaviTeja25@users.noreply.github.com>
2021-09-08 15:04:08 +0530

    Add files via upload

---
+++ 3_Implementation/Src/error.c
@@ -0,0 +1,23 @@
+/**
+ * @file error.c
+ * @author Raviteja
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+ 
+
+   void errorMessage() 
+{
+    printf("+++!!!You selected invalid number!!!+++\n");
+}
